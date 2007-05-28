45536697a4a5280e2c35aa8cac7ab6d38debfb5b
hans <hans@fugal.net>
2007-05-27 19:17:20 -0600

    leg
    darcs-hash:20070528011720-28bda-40e2d099f80f9d91c3d714889f03d1ca4473f51f.gz

---
+++ lib/lindbergh/leg.rb
@@ -0,0 +1,9 @@
+require 'ostruct'
+require 'lindbergh/waypoint'
+
+# One leg of a flight plan. Consists of a beginning and ending Waypoint, and
+# has all the interesting variables associated with it, like airspeed, wind,
+# etc.
+class Leg < OpenStruct
+end
+
